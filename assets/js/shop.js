e60c9dbc22c7ef7a6cd816fc7d0596bf7b088598
Miguel Nicolas Gimenez Aviles <nickgyta@gmail.com>
2022-06-16 12:08:42 -0300

    Agregando items al carrito/OBJETOS

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -12,12 +12,13 @@ class item{
         console.log('Has seleccionado el siguiente item: ' + this.tipo + ' talla ' + this.talle + ', de marca ' + this.marca + ' y de color ' + this.color+' al costo de $'+this.precio+' pesos.')
     }
     sumaIva(){
-        this.precio = this.precio * 1.21;
-        console.log(this.precio)
+        this.precio *= 1.21;
+        console.log(`con el IVA incluido el artículo ${this.tipo} se va a ${this.precio}`)
     }
     vender(){
         this.vendido=true
     }
+
 }
 const item1 = new item('Remera', 'M', 'Reebok', 'Negro', 3500)
 const item2 = new item('Short', 'L', 'Reebok', 'Azul', 2800)
@@ -25,39 +26,18 @@ const item3 = new item('Zapatillas', 42, 'Reebok', 'Negro', 18000)
 const item4 = new item('Medias', 'M', 'Reebok', 'Azul', 1000)
 const item5 = new item('Calleras', 'L', 'Rogue', 'Negro', 1400)
 
+    
 item1.carrito();
-item1.sumaIva();
 item2.carrito();
 item3.carrito();
 item4.carrito();
 item5.carrito();
 
-// }
-// const item1 = {
-//     tipo: 'short',
-//     talle: 'l',
-//     marca: 'reebok',
-//     color: 'azul',
+item1.sumaIva();
+item2.sumaIva();
+item3.sumaIva();
+item4.sumaIva();
+item5.sumaIva();
 
-// }
-// const item2 = {
-//     tipo: 'zapatillas',
-//     talle: '42',
-//     marca: 'reebok',
-//     color: 'negro',
-
-// }
-// const item3 = {
-//     tipo: 'medias',
-//     talle: 'm',
-//     marca: 'reebok',
-//     color: 'azul',
-
-// }
-// const item4 = {
-//     tipo: 'callera',
-//     talle: 'l',
-//     marca: 'rogue',
-//     color: 'negro',
-
-// }
+const total = item1.precio+item2.precio+item3.precio+item4.precio+item5.precio;
+console.log(`El precio total de tus productos seleccionados es de $ ${total} pesos`)
