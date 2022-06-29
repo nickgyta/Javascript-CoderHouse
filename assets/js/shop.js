dd132c174a38b4dc651caed29b91912db2129623
Miguel Nicolas Gimenez Aviles <nickgyta@gmail.com>
2022-06-29 17:59:22 -0300

    Agregando carrito, solucionando el código y aplicando  Eventos

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,191 +1,261 @@
 // E-COMMERCE del GYM
+// const carrito = [];
+// let total = 0;
+
+    const storeContainer = document.querySelector('#store');
+    const carritoContenedor = document.querySelector('#carrito-contenedor')
+
+    const contadorCarrito = document.querySelector('#contadorCarrito')
+    const precioTotal = document.querySelector('#precioTotal')
+    
+    const btnVaciarCarrito = document.querySelector('#vaciarCarrito')
+
+    let carrito = []
+
+    items.forEach((item)=>{
+        const div = document.createElement('div')
+    div.classList.add('card')
+
+    div.innerHTML = `<div class="d-flex justify-content-between p-3">
+                    <p class="lead mb-0">Oferta del Dia</p>
+                    <div class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style="width: 35px; height: 35px;">
+                    <p class="text-white mb-0 small">${item.cantidad}</p>
+                    </div>
+                    </div>
+                    <img src="${item.imagen}"
+                    class="card-img-top" alt="Laptop" />
+                    <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <p class="small"><a href="#!" class="text-muted">Indumentaria de entrenamiento</a></p>
+                        <p class="small text-danger"><s>$${item.precio} </s></p>
+                    </div>
+                    <div class="d-flex justify-content-between mb-3">
+                        <h5 class="mb-0">${item.tipo}, ${item.marca}<hr>Color ${item.color}<hr>Talle ${item.talle}</h5>
+                        <h5 class="text-dark mb-0">$${item.promo}</h5>
+                    </div>
+                    <div class="d-flex justify-content-between mb-2">
+                        <p class="text-muted mb-0">Disponibles: <span class="fw-bold">6</span></p>
+                        <div class="ms-auto text-warning">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        </div>
+                    </div><a href="#!" class="btn btn-primary" onclick='agregarItemAlCarrito(${item.iD})'>Agregar al carrito  <i class="fas fa-shopping-cart"></i></a>
+                    </div>
+                    </div>
+                    `
+
+    storeContainer.append(div)
+    })
+
+    const agregarItemAlCarrito = (iD) => {
+        const prod = items.find( (item) => item.iD === iD) 
+        carrito.push(prod)
+
+        console.log(carrito)
+        renderCarrito()
+        visualizarCantidad()
+        renderTotal()
+    }
+
+    const removerCarrito = (iD) => {
+        const remover = carrito.find((item) => item.iD === iD)
+        const indice = carrito.indexOf(remover)
+        carrito.splice(indice, 1)
+
+        console.log(carrito)
+        renderCarrito()
+        visualizarCantidad()
+        renderTotal()
+    }
+    const VaciarCarrito = () => {
+    carrito = []
+
+        renderCarrito()
+        visualizarCantidad()
+        renderTotal()
+    }
+
+    btnVaciarCarrito.addEventListener('click', VaciarCarrito)
+    
+    const renderCarrito = () => {
+        carritoContenedor.innerHTML = ''
+
+        carrito.forEach((art)=>{
+            const div = document.createElement('div')
+            div.classList.add('productoEnCarrito')
+
+            div.innerHTML = `
+                        <p>${art.tipo}</p>
+                        <p>Precio: $${art.promo}</p>
+                        <button onclick='removerCarrito(${art.iD})'class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                        `
+
+            carritoContenedor.append(div)
+        })
+    }
+
+    const visualizarCantidad = () => {
+        contadorCarrito.innerText = carrito.length
+    }
+
+    const renderTotal = () => {
+        let total = 0
+        carrito.forEach((art) => {
+            total += art.precio
+        }) 
+        precioTotal.innerText = total
+    }
 
 
 
 
-const item = [{
-    'iD': 1,
-    'tipo': 'Remera',
-    'talle': 'M',
-    'marca': 'Reebok',
-    'color': 'Negro',
-    'precio': 3500,
-    'promo': 2999,
-    'cantidad': 1,
-    'imagen': 'https://http2.mlstatic.com/D_NQ_NP_2X_703026-MLA46375750218_062021-F.webp',
-},
-{
-    'iD': 2,
-    'tipo': 'Short',
-    'talle': 'M',
-    'marca': 'Reebok',
-    'color': 'Azul',
-    'precio': 2800,
-    'promo': 2150,
-    'cantidad': 1,
-    'imagen': 'https://http2.mlstatic.com/D_NQ_NP_385115-MLM25146337841_112016-O.jpg',
-},
-{
-    'iD': 3,
-    'tipo': 'Zapatillas',
-    'talle': '42',
-    'marca': 'Reebok',
-    'color': 'Negro',
-    'precio': 18000,
-    'promo': 14800,
-    'cantidad': 1,
-    'imagen': 'https://photo2.i-run.fr/reebok-crossfit-nano-6.0-m-chaussures-homme-132655-1-z.jpg',
-},
-{
-    'iD': 4,
-    'tipo': 'Medias',
-    'talle': 'M',
-    'marca': 'Reebok',
-    'color': 'Azul',
-    'precio': 1000,
-    'promo': 850,
-    'cantidad': 1,
-    'imagen': 'https://http2.mlstatic.com/D_NQ_NP_2X_711320-MLV32036388683_092019-F.webp',
-},
-{
-    'iD': 5,
-    'tipo': 'Calleras',
-    'talle': 'L',
-    'marca': 'Rogue',
-    'color': 'Negro',
-    'precio': 1400,
-    'promo': 1150,
-    'cantidad': 1,
-    'imagen': 'https://http2.mlstatic.com/D_NQ_NP_2X_686109-MLA49108573513_022022-F.webp',
-}
-]
-const carrito = [];
-let total = 0;
 
-function gestionarItems() {
-    let store = document.querySelector('#store');
-    let filtro = document.getElementById('filtro');
-    filtro.innerHTML = `
-    <button class= 'btn btn-warning mb-5' onclick='filtroPrecio()'>filtrar mayor 70</button>
-    `
-    item.forEach((e) => {
-        let productoHTML = `
-            <div class="d-flex justify-content-between p-3">
-            <p class="lead mb-0">Oferta del Dia</p>
-            <div class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style="width: 35px; height: 35px;">
-            <p class="text-white mb-0 small">${e.cantidad}</p>
-          </div>
-        </div>
-        <img src="${e.imagen}"
-          class="card-img-top" alt="Laptop" />
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <p class="small"><a href="#!" class="text-muted">Indumentaria de entrenamiento</a></p>
-            <p class="small text-danger"><s>${e.precio} </s></p>
-          </div>
-          <div class="d-flex justify-content-between mb-3">
-            <h5 class="mb-0">${e.tipo}, ${e.marca}<hr>Color ${e.color}<hr>Talle ${e.talle}</h5>
-            <h5 class="text-dark mb-0">${e.promo}</h5>
-          </div>
-          <div class="d-flex justify-content-between mb-2">
-            <p class="text-muted mb-0">Disponibles: <span class="fw-bold">6</span></p>
-            <div class="ms-auto text-warning">
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-            </div>
-          </div><a href="#!" class="btn btn-primary" onclick='agregarItemAlCarrito ${e.iD}'>Agregar al carrito</a>
-        </div>
-        </div>
-        `
-        store.innerHTML += productoHTML
-    });
-}
-gestionarItems();
 
-function agregarItemAlCarrito(iD){
-    let producto = item.find(producto=> producto.iD == iD);
-    let productoEnCarrito = carrito.find(producto => producto.iD == iD);
-    if(productoEnCarrito){
-        productoEnCarrito.cantidad++;
-    }else{
-        producto.cantidad = 1;
-        carrito.push(producto);
+
+    // function gestionarItems() {
+    // const store = document.querySelector('#store');
+    // let filtro = document.getElementById('filtro');
+    // filtro.innerHTML = `
+    // <button class= 'btn btn-warning mb-5' onclick='filtroPrecio()'>filtrar mayor a $10.000</button>
+    // `
+//     item.forEach((e) => {
+//         let productoHTML = `
+//             <div class="d-flex justify-content-between p-3">
+//             <p class="lead mb-0">Oferta del Dia</p>
+//             <div class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style="width: 35px; height: 35px;">
+//             <p class="text-white mb-0 small">${e.cantidad}</p>
+//           </div>
+//         </div>
+//         <img src="${e.imagen}"
+//           class="card-img-top" alt="Laptop" />
+//         <div class="card-body">
+//           <div class="d-flex justify-content-between">
+//             <p class="small"><a href="#!" class="text-muted">Indumentaria de entrenamiento</a></p>
+//             <p class="small text-danger"><s>${e.precio} </s></p>
+//           </div>
+//           <div class="d-flex justify-content-between mb-3">
+//             <h5 class="mb-0">${e.tipo}, ${e.marca}<hr>Color ${e.color}<hr>Talle ${e.talle}</h5>
+//             <h5 class="text-dark mb-0">${e.promo}</h5>
+//           </div>
+//           <div class="d-flex justify-content-between mb-2">
+//             <p class="text-muted mb-0">Disponibles: <span class="fw-bold">6</span></p>
+//             <div class="ms-auto text-warning">
+//               <i class="fa fa-star"></i>
+//               <i class="fa fa-star"></i>
+//               <i class="fa fa-star"></i>
+//               <i class="fa fa-star"></i>
+//               <i class="fa fa-star"></i>
+//             </div>
+//           </div><a href="#!" class="btn btn-primary" onclick='agregarItemAlCarrito ${e.iD}'>Agregar al carrito<i class="fas fa-shopping-cart"></i></a>
+//         </div>
+//         </div>
+//         `
+//         store.innerHTML += productoHTML
+//     });
+// }
+// gestionarItems();
+
+// function agregarItemAlCarrito(iD){
+//     let producto = item.find(producto=> producto.iD == iD);
+//     let productoEnCarrito = carrito.find(producto => producto.iD == iD);
+//     if(productoEnCarrito){
+//         productoEnCarrito.cantidad+producto.cantidad;
+//     }else{
+//         producto.cantidad = 1;
+//         carrito.push(producto);
         
-    }
-    gestionarCarrito();
-}
+//     }
+//     gestionarCarrito();
+// }
 
-function gestionarCarrito(){
+// function gestionarCarrito(){
 
-let carritoHTML = document.querySelectorAll('#carrito')
+// let carritoHTML = document.getElementById('carrito')
 
- html = '';
- carrito.forEach((producto, iD)=>{
-    html += `
-    <div class="d-flex justify-content-between p-3">
-    <p class="lead mb-0">Oferta del Dia</p>
-    <div class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style="width: 35px; height: 35px;">
-    <p class="text-white mb-0 small">${e.cantidad}</p>
-  </div>
-</div>
-<img src="${e.imagen}"
-  class="card-img-top" alt="Laptop" />
-<div class="card-body">
-  <div class="d-flex justify-content-between">
-    <p class="small"><a href="#!" class="text-muted">Indumentaria de entrenamiento</a></p>
-    <p class="small text-danger"><s>${e.precio} </s></p>
-  </div>
-  <div class="d-flex justify-content-between mb-3">
-    <h5 class="mb-0">${e.tipo}, ${e.marca}<hr>Color ${e.color}<hr>Talle ${e.talle}</h5>
-    <h5 class="text-dark mb-0">${e.promo}</h5>
-  </div>
-  <div class="d-flex justify-content-between mb-2">
-    <p class="text-muted mb-0">Disponibles: <span class="fw-bold">6</span></p>
-    <div class="ms-auto text-warning">
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-    </div>
-  </div><a href="#!" class="btn btn-primary" onclick='eliminarItemDelCarrito ${e.iD}'>Agregar al carrito</a>
-</div>
-</div>
-`
- })
- calcularTotal();
-carritoHTML.innerHTML = html; 
-}
+//  html = '';
+//  carrito.forEach((producto, iD)=>{
+//     html += `
+//     <div class="d-flex justify-content-between p-3">
+//     <p class="lead mb-0">Oferta del Dia</p>
+//     <div class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong" style="width: 35px; height: 35px;">
+//     <p class="text-white mb-0 small">${producto.cantidad}</p>
+//   </div>
+// </div>
+// <img src="${producto.imagen}"
+//   class="card-img-top" alt="Laptop" />
+// <div class="card-body">
+//   <div class="d-flex justify-content-between">
+//     <p class="small"><a href="#!" class="text-muted">Indumentaria de entrenamiento</a></p>
+//     <p class="small text-danger"><s>${producto.precio} </s></p>
+//   </div>
+//   <div class="d-flex justify-content-between mb-3">
+//     <h5 class="mb-0">${producto.tipo}, ${producto.marca}<hr>Color ${producto.color}<hr>Talle ${producto.talle}</h5>
+//     <h5 class="text-dark mb-0">${producto.promo}</h5>
+//   </div>
+//   <div class="d-flex justify-content-between mb-2">
+//     <p class="text-muted mb-0">Disponibles: <span class="fw-bold">6</span></p>
+//     <div class="ms-auto text-warning">
+//       <i class="fa fa-star"></i>
+//       <i class="fa fa-star"></i>
+//       <i class="fa fa-star"></i>
+//       <i class="fa fa-star"></i>
+//       <i class="fa fa-star"></i>
+//     </div>
+//   </div><a href="#!" class="btn btn-primary" onclick='eliminarItemDelCarrito ${iD}'>Agregar al carrito</a>
+// </div>
+// </div>
+// `
+//  })
+//  calcularTotal();
+// carritoHTML.innerHTML = html; 
+
+// }
 
 
-function calcularTotal(){
+// function calcularTotal(){
 
-    carrito.forEach((producto)=>{
+//     carrito.forEach((producto)=>{
 
-        total += producto.promo * producto.cantidad;
-    });
-    console.log(total);
-}
+//         total += producto.promo * producto.cantidad;
+//     });
+//     console.log(total);
+// }
 
-const eliminarItemDelCarrito = (iD) => {
-    console.log(carrito[iD].cantidad);
-    carrito[iD].cantidad--;
-    console.log(carrito[iD].cantidad);
+// const eliminarItemDelCarrito = (iD) => {
+//     console.log(carrito[iD].cantidad);
+//     carrito[iD].cantidad--;
+//     console.log(carrito[iD].cantidad);
 
-    if(carrito[iD].cantidad== 0){
-        carrito.splice(iD, 1);
-    }
-    gestionarCarrito();
-}
+//     if(carrito[iD].cantidad== 0){
+//         carrito.splice(iD, 1);
+//     }
+//     gestionarCarrito();
+// }
 
-function filtroPrecio(){
-    let bd = item.filter(producto=>producto.promo>70);
-    console.log(bd);
+// function filtroPrecio(){
+//     let bd = item.filter(producto=>producto.promo>10000);
+//     console.log(bd);
 
-}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //     carrito(){
 //         console.log('Has seleccionado el siguiente item: ' + this.tipo + ' talla ' + this.talle + ', de marca ' + this.marca + ' y de color ' + this.color+' al costo de $'+this.precio+' pesos.')
 //     }
--- index.js
+++ index.js
@@ -1,95 +1,95 @@
 
 // ARMANDO CONDICIONAL PARA PAGINA WEB DEL GIMNASIO // CONDICIONAL IF
 
-let person = prompt('Ingresa su nombre y apellido')
+// let person = prompt('Ingresa su nombre y apellido')
 
-alert('Bienvenido ' + person)
+// alert('Bienvenido ' + person)
 
-let training = prompt('Ingrese el tipo de entrenamiento que ud. realiza(libre/fijo/programacion')
+// let training = prompt('Ingrese el tipo de entrenamiento que ud. realiza(libre/fijo/programacion')
 
-if(training === 'libre'){
-    alert('Bienvenido a las clases libres de IronCross ' + person+'.')
-}
-else if(training === 'fijo'){
-    alert('Bienvenido a las clases guiadas semanales de IronCross ' + person+'.')
-}
-else if(training === 'programacion'){
-    alert('Bienvenido a las clases de entrenamiento hiit y musculacion de IronCross ' + person+'.')
-}
-else{
-    alert('No es una clase válida. Por favor ingrese un tipo de clase de entrenamiento válido')
-}
-
-
+// if(training === 'libre'){
+//     alert('Bienvenido a las clases libres de IronCross ' + person+'.')
+// }
+// else if(training === 'fijo'){
+//     alert('Bienvenido a las clases guiadas semanales de IronCross ' + person+'.')
+// }
+// else if(training === 'programacion'){
+//     alert('Bienvenido a las clases de entrenamiento hiit y musculacion de IronCross ' + person+'.')
+// }
+// else{
+//     alert('No es una clase válida. Por favor ingrese un tipo de clase de entrenamiento válido')
+// }
 
 
-//Mi deuda del gimnasio
 
 
-let mesActual = 6
-let mesesPagados= Number(prompt('indique la cantidad de meses pagados en lo que va del año'))
-let mesesDeudores  = mesActual - mesesPagados
+// //Mi deuda del gimnasio
 
 
-while(mesesDeudores === 0){
- mesesDeudores+=0
- break
-alert('estas al dia con tu cuota')
+// let mesActual = 6
+// let mesesPagados= Number(prompt('indique la cantidad de meses pagados en lo que va del año'))
+// let mesesDeudores  = mesActual - mesesPagados
+
+
+// while(mesesDeudores === 0){
+//  mesesDeudores+=0
+//  break
+// alert('estas al dia con tu cuota')
  
 
-}if (mesesDeudores > 0){
-    alert('estas debiendo '+ mesesDeudores + ' cuota/s')
+// }if (mesesDeudores > 0){
+//     alert('estas debiendo '+ mesesDeudores + ' cuota/s')
 
-}else{
+// }else{
     
-    alert('llevas adelantadas ' + mesesDeudores +' cuota/s') 
-    }
+//     alert('llevas adelantadas ' + mesesDeudores +' cuota/s') 
+//     }
 
-// ARRAYS - ORDENAR NUMERO DE LOS PARTICIPANTES CLASIFICADOS PARA LA PROXIMA COMPETENCIA DE CROSSFIT DE LA PROVINCIA
+// // ARRAYS - ORDENAR NUMERO DE LOS PARTICIPANTES CLASIFICADOS PARA LA PROXIMA COMPETENCIA DE CROSSFIT DE LA PROVINCIA
 
 
-let numeroos = [9, 11, 6, 2, 17, 13, 32, 27]
-numeroos.sort((a,b) =>{
-    if(a === b){
-        return 0;
-    }
-    if(a<b){
-        return -1;
-    }
-    else{
-        return 1;
-    }
-});
-console.log(numeroos)
+// let numeroos = [9, 11, 6, 2, 17, 13, 32, 27]
+// numeroos.sort((a,b) =>{
+//     if(a === b){
+//         return 0;
+//     }
+//     if(a<b){
+//         return -1;
+//     }
+//     else{
+//         return 1;
+//     }
+// });
+// console.log(numeroos)
 
-// ORDENAR PARTICIPANTES CLASIFICADOS DEL ULTIMO AL PRIMER PUESTO
+// // ORDENAR PARTICIPANTES CLASIFICADOS DEL ULTIMO AL PRIMER PUESTO
 
-let numeros = [65, 32, 85, 47, 12, 3, 99, 55]
-numeros.sort((a,b) =>{
-    if(a === b){
-        return 1
-    }
-    else if (a<b){
-        return 1
-    }
-    else{
-        return -1
-    }
-})
-console.log(numeros)
+// let numeros = [65, 32, 85, 47, 12, 3, 99, 55]
+// numeros.sort((a,b) =>{
+//     if(a === b){
+//         return 1
+//     }
+//     else if (a<b){
+//         return 1
+//     }
+//     else{
+//         return -1
+//     }
+// })
+// console.log(numeros)
 
-// ORDENAR EL TOTAL DE PARTICIPANTES YA CLASIFICADOS ANTERIORMENTE
+// // ORDENAR EL TOTAL DE PARTICIPANTES YA CLASIFICADOS ANTERIORMENTE
 
-let numbers = [14,6,98,52,64,31,78,99,5,12,32,71,2,0]
-numbers.sort((a,b)=>{
-    if(a===b){
-        return 0
-    }
-    else if(a<b){
-        return -1
-    }
-    else{
-        return 1
-    }
-})
-console.log(numbers)
+// let numbers = [14,6,98,52,64,31,78,99,5,12,32,71,2,0]
+// numbers.sort((a,b)=>{
+//     if(a===b){
+//         return 0
+//     }
+//     else if(a<b){
+//         return -1
+//     }
+//     else{
+//         return 1
+//     }
+// })
+// console.log(numbers)
